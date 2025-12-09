697095c59671fc58fd3bafb838f1b12a7c7f593e
sangwon <chosw0410@naver.com>
2025-12-09 18:01:16 +0900

    [fix] UTC 인것들 변경

--- backend/src/main/java/com/synerge/order101/shipment/model/service/ShipmentService.java
+++ backend/src/main/java/com/synerge/order101/shipment/model/service/ShipmentService.java
@@ -40,7 +40,7 @@ public class ShipmentService {
         // 일단 테스트 용으로 5분 설정.
         int w2t = shipmentRepository.updateFromCreatedAt(
                 ShipmentStatus.WAITING, ShipmentStatus.IN_TRANSIT,
-                now.minusMinutes(5), now
+                now.minusMinutes(3), now
         );
 
         if (w2t > 0) {
@@ -58,7 +58,7 @@ public class ShipmentService {
         // 일단 테스트 용으로 30분 설정.
         int t2d = shipmentRepository.updateFromUpdatedAt(
                 ShipmentStatus.IN_TRANSIT, ShipmentStatus.DELIVERED,
-                now.minusMinutes(30), now
+                now.minusMinutes(5), now
         );
 
         if (t2d > 0) {
