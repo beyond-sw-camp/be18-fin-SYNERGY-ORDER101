6dd6b040e1df7476b199f8f22b671d3b3e6133ed
sangwon <chosw0410@naver.com>
2025-12-11 02:04:21 +0900

    [feat] 챗봇 컨트롤러 - JAVA

---
+++ backend/src/main/java/com/synerge/order101/chatbot/controller/ChatController.java
@@ -0,0 +1,46 @@
+package com.synerge.order101.chatbot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final WebClient webClient;
+
+    @GetMapping("/meta")
+    public Mono<String> meta() {
+        return webClient
+                .get()
+                .uri("/api/v1/chat/meta")
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @PostMapping(value = "/stream", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Flux<String> stream(@RequestBody Map<String, Object> body) {
+
+        return webClient.post()
+                .uri("/api/v1/chat/stream")  
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToFlux(DataBuffer.class)
+                .map(buffer -> {
+                    byte[] bytes = new byte[buffer.readableByteCount()];
+                    buffer.read(bytes);
+                    return new String(bytes, StandardCharsets.UTF_8);
+                });
+    }
+}
