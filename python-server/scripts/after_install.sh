077c831fd95533ff5f623ee92d891b65835efbfa
sangwon <chosw0410@naver.com>
2025-12-11 15:23:38 +0900

    [fix] 스크립트

--- python-server/scripts/after_install.sh
+++ python-server/scripts/after_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "AfterInstall: preparing..."
 
+set +e
+
 cd /home/ec2-user/python-server
 chmod -R 755 scripts
 
+exit 0
--- python-server/scripts/before_install.sh
+++ python-server/scripts/before_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "BeforeInstall: cleaning old containers"
 
+set +e
+
 docker stop python-server || true
 docker rm python-server || true
 
 mkdir -p /home/ec2-user/python-server
+
+exit 0
