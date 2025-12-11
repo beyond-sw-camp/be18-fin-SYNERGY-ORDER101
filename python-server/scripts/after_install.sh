b29aa27276cae59b487cbc4617e05ae158363e6d
sangwon <chosw0410@naver.com>
2025-12-11 15:49:05 +0900

    [fix] py aws

--- python-server/scripts/after_install.sh
+++ python-server/scripts/after_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "AfterInstall: preparing..."
 
-set +e
-
 cd /home/ec2-user/python-server
-chmod -R 755 scripts
 
-exit 0
+# 디렉토리 권한 수정 (여기 추가)
+chown -R ec2-user:ec2-user /home/ec2-user/python-server
+chmod -R 755 /home/ec2-user/python-server
+
+chmod -R 755 scripts
